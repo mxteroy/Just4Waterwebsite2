2e258ab242828d6f845442f16b752188cb13a880
James <mxteroy@gmail.com>
2018-10-08 15:01:34 -0500

    mobile test

--- assets/js/ripple.js
+++ assets/js/ripple.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function(){
     
     "use strict"
     $('#page').ripples({
-      dropRadius: 20,
-      perturbance: .0001,
+      dropRadius: 30,
+      perturbance: .01,
 });
 });
