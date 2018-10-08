20d42d71f2766b1a9ad6d4585b5f7d4500269548
James <mxteroy@gmail.com>
2018-10-08 13:20:33 -0500

    ripple and icon change

--- assets/js/ripple.js
+++ assets/js/ripple.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function(){
     
     "use strict"
     $('#page').ripples({
-      dropRadius: 50,
-      perturbance: 0.20,
+      dropRadius: 20,
+      perturbance: .15,
 });
 });
