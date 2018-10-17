6b79ddd4f09a7d470f329b30c027be420b89f4d4
James <mxteroy@gmail.com>
2018-10-17 18:20:57 -0500

    Cleaned up the stuff

--- assets/js/ripple.js
+++ assets/js/ripple.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function(){
     
     "use strict"
     $('#page').ripples({
-      dropRadius: 30,
-      perturbance: .01,
+      dropRadius: 50,
+      perturbance: 10,
 });
 });
